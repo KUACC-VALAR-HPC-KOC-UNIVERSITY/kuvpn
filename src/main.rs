0d3e86119a776d2bb0704ea689e25e947f39bf1b
Eray Altun <nyverin@gmail.com>
2024-08-27 09:49:43 +0300

    Refined autologin

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod driver;
 use args::Args;
 use clap::Parser;
 use driver::{Driver, DriverError};
-use fantoccini::ClientBuilder;
+use fantoccini::{error::ErrorStatus, ClientBuilder};
 use std::env;
 use tokio::time::{sleep, Duration};
 
@@ -25,7 +25,7 @@ async fn main() -> Result<(), DriverError> {
             Ok(_) => break,
             Err(err) => match err {
                 DriverError::WebDriverConnectionError(e) => {
-                    println!("WebDriverConnectionError encountered: {}. Retrying...", e);
+                    println!("WebDriverConnectionError encountered: {:#?}. Retrying...", e);
                     attempt_count += 1;
                     if attempt_count > 3 {
                         println!("Exceeded maximum retry attempts for WebDriver connection.");
@@ -69,7 +69,17 @@ async fn run_client(url: String, port: u16) -> Result<(), DriverError> {
 
     perform_autologin(&c).await?;
 
-    if let Some(cookie_value) = get_dsid_cookie(&c).await? {
+    let mut cookie_value = None;
+    let start_time = std::time::Instant::now();
+    while start_time.elapsed() < Duration::from_secs(30) {
+        if let Some(value) = get_dsid_cookie(&c).await? {
+            cookie_value = Some(value);
+            break;
+        }
+        sleep(Duration::from_millis(100)).await; // Wait before retrying
+    }
+
+    if let Some(cookie_value) = cookie_value {
         // Close the WebDriver client immediately
         c.close().await?;
 
@@ -82,50 +92,73 @@ async fn run_client(url: String, port: u16) -> Result<(), DriverError> {
     Ok(())
 }
 
-async fn perform_autologin(c: &fantoccini::Client) -> Result<(), DriverError> {
-    if let (Ok(username), Ok(password)) = (env::var("KUVPN_USERNAME"), env::var("KUVPN_PASSWORD")) {
-        // Wait for the email input field to appear and then fill it
-        wait_and_send_keys(
-            c,
-            "input[name='loginfmt']",
-            &username,
-            Duration::from_secs(10),
-        )
-        .await;
-
-        // Click the "Next" button
-        wait_and_click(c, "#idSIButton9", Duration::from_secs(10)).await;
-
-        // Wait for the password input field to appear and then fill it
-        wait_and_send_keys(
-            c,
-            "input[name='passwd']",
-            &password,
-            Duration::from_secs(10),
-        )
-        .await;
-
-        // Click the "Sign in" button
-        wait_and_click(c, "#idSIButton9", Duration::from_secs(10)).await;
-
-        // Click the "No: For Stay Signed in?" button as, it will not remember anyways
-        wait_and_click(c, "#idSIButton9", Duration::from_secs(60)).await;
-
-        // Execute gowelcome(); until it fails or runs 2 times correctly
-        let mut successful_executions = 0;
-        while successful_executions < 2 {
-            match c.execute("gowelcome();", vec![]).await {
-                Ok(_) => successful_executions += 1,
-                Err(_) => break,
-            }
+async fn perform_autologin(c: &fantoccini::Client) -> Result<Option<String>, DriverError> {
+    let (username, password) = match (env::var("KUVPN_USERNAME"), env::var("KUVPN_PASSWORD")) {
+        (Ok(u), Ok(p)) if !u.is_empty() && !p.is_empty() => (u, p),
+        _ => {
+            println!("KUVPN_USERNAME and/or KUVPN_PASSWORD not set or empty. Skipping login process.");
+            return Ok(None);
         }
+    };
 
-        // Check if we're on the confirmation page for multiple sessions TODO
-        // wait_and_click(c, "#btnContinue", Duration::from_secs(1)).await;
-    } else {
-        println!("KUVPN_USERNAME and/or KUVPN_PASSWORD not set. Skipping login process.");
+    // Wait for the email input field to appear and then fill it
+    match wait_and_send_keys(
+        c,
+        "input[name='loginfmt']",
+        &username,
+        Duration::from_secs(10),
+    )
+    .await? {
+        WaitRes::FoundCookie(cookie) => return Ok(Some(cookie)),
+        _ => {}
     }
-    Ok(())
+
+    // Click the "Next" button
+    match wait_and_click(c, "#idSIButton9", Duration::from_secs(10)).await? {
+        WaitRes::FoundCookie(cookie) => return Ok(Some(cookie)),
+        _ => {}
+    }
+
+    // Wait for the password input field to appear and then fill it
+    match wait_and_send_keys(
+        c,
+        "input[name='passwd']",
+        &password,
+        Duration::from_secs(10),
+    )
+    .await? {
+        WaitRes::FoundCookie(cookie) => return Ok(Some(cookie)),
+        _ => {}
+    }
+
+    // Click the "Sign in" button
+    match wait_and_click(c, "#idSIButton9", Duration::MAX).await? {
+        WaitRes::FoundCookie(cookie) => return Ok(Some(cookie)),
+        _ => {}
+    }
+
+    // Click the "No: For Stay Signed in?" button as, it will not remember anyways
+    match wait_and_click(c, "#idSIButton9", Duration::MAX).await? {
+        WaitRes::FoundCookie(cookie) => return Ok(Some(cookie)),
+        _ => {}
+    }
+
+    // Execute gowelcome(); until it fails or runs 2 times correctly
+    let mut successful_executions = 0;
+    while successful_executions < 2 {
+        match c.execute("gowelcome();", vec![]).await {
+            Ok(_) => successful_executions += 1,
+            Err(_) => break,
+        }
+    }
+
+    // Check if we're on the confirmation page for multiple sessions
+    match wait_and_click(c, "#btnContinue", Duration::from_secs(30)).await? {
+        WaitRes::FoundCookie(cookie) => return Ok(Some(cookie)),
+        _ => {}
+    }
+
+    Ok(None)
 }
 
 async fn get_dsid_cookie(c: &fantoccini::Client) -> Result<Option<String>, DriverError> {
@@ -153,39 +186,71 @@ fn execute_openconnect(cookie_value: String) -> Result<(), DriverError> {
     Ok(())
 }
 
+enum WaitRes {
+    Found,
+    TimeOut,
+    FoundCookie(String),
+}
+
 async fn wait_and_send_keys(
     c: &fantoccini::Client,
     selector: &str,
     keys: &str,
     timeout: Duration,
-) -> bool {
+) -> Result<WaitRes, DriverError> {
     let start_time = std::time::Instant::now();
     while start_time.elapsed() < timeout {
+        if let Some(cookie) = get_dsid_cookie(c).await? {
+            return Ok(WaitRes::FoundCookie(cookie));
+        }
         match c.find(fantoccini::Locator::Css(selector)).await {
             Ok(element) => {
                 if let Ok(_) = element.send_keys(keys).await {
-                    return true;
+                    return Ok(WaitRes::Found);
                 }
             }
-            Err(_) => {}
+            Err(fantoccini::error::CmdError::NotJson(_)) => {}
+            Err(fantoccini::error::CmdError::Json(_)) => {}
+            Err(fantoccini::error::CmdError::InvalidArgument(_, _)) => {}
+            Err(fantoccini::error::CmdError::Standard(e)) => {
+                if e.error == ErrorStatus::NoSuchElement || e.error == ErrorStatus::NoSuchWindow {
+                    continue;
+                } else {
+                    return Err(driver::DriverError::WebDriverConnectionError(fantoccini::error::CmdError::Standard(e)));
+                }
+            }
+            Err(e) => return Err(driver::DriverError::WebDriverConnectionError(e)),
         }
         sleep(Duration::from_millis(150)).await;
     }
-    false
+    Ok(WaitRes::TimeOut)
 }
 
-async fn wait_and_click(c: &fantoccini::Client, selector: &str, timeout: Duration) -> bool {
+async fn wait_and_click(c: &fantoccini::Client, selector: &str, timeout: Duration) -> Result<WaitRes, DriverError> {
     let start_time = std::time::Instant::now();
     while start_time.elapsed() < timeout {
+        if let Some(cookie) = get_dsid_cookie(c).await? {
+            return Ok(WaitRes::FoundCookie(cookie));
+        }
         match c.find(fantoccini::Locator::Css(selector)).await {
             Ok(element) => {
                 if let Ok(_) = element.click().await {
-                    return true;
+                    return Ok(WaitRes::Found);
                 }
             }
-            Err(_) => {}
+            Err(fantoccini::error::CmdError::NotJson(_)) => {}
+            Err(fantoccini::error::CmdError::Json(_)) => {}
+            Err(fantoccini::error::CmdError::InvalidArgument(_, _)) => {}
+            Err(fantoccini::error::CmdError::Standard(e)) => {
+                if e.error == ErrorStatus::NoSuchElement || e.error == ErrorStatus::NoSuchWindow {
+                    continue;
+                } else {
+                    return Err(driver::DriverError::WebDriverConnectionError(fantoccini::error::CmdError::Standard(e)));
+                }
+            }
+            Err(e) => return Err(driver::DriverError::WebDriverConnectionError(e)),
         }
         sleep(Duration::from_millis(500)).await;
     }
-    false
+    Ok(WaitRes::TimeOut)
 }
