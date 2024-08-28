cd03768f577a8c63bf4e7beaa0dccd960c9ec393
Eray Altun <nyverin@gmail.com>
2024-08-28 15:35:19 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         # Define the default package
         packages.default = rustPlatform.buildRustPackage rec {
           pname = "kuvpn";
-          version = "0.5.0";
+          version = "0.5.1";
 
           src = ./.;
 
