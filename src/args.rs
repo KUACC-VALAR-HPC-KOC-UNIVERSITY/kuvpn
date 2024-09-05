8d9686007b929148acbf303d8fc51c1b1e7ce1c5
Eray Altun <nyverin@gmail.com>
2024-09-05 21:07:57 +0300

    Init Commit

--- src/args.rs
+++ src/args.rs
@@ -18,10 +18,6 @@ pub struct Args {
     #[arg(short, long, default_value = "https://vpn.ku.edu.tr")]
     pub url: String,
 
-    /// The port number for the ChromeDriver
-    #[arg(short, long, default_value_t = 9515)]
-    pub port: u16,
-
     /// The level of logging
     #[arg(short, long, value_enum, default_value_t = LogLevel::Info)]
     pub level: LogLevel,
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,75 @@
 mod args;
-mod client;
-mod driver;
 mod logger;
-mod utils;
 
-use crate::logger::init_logger;
 use args::Args;
 use clap::Parser;
-use client::run_client_with_retries;
-use driver::{Driver, DriverError};
-use tokio::time::Duration;
-use utils::check_dependencies;
+use headless_chrome::{Browser, LaunchOptions};
+use logger::init_logger;
+use std::env;
+use std::error::Error;
+use std::ffi::OsString;
+use std::fs;
+use std::path::PathBuf;
+use std::thread;
+use std::time::Duration;
 
-#[tokio::main]
-async fn main() -> Result<(), DriverError> {
-    let mut args = Args::parse();
+fn main() {
+    let args = Args::parse();
+
     init_logger(&args.level);
 
-    check_dependencies(args.dsid)?;
-    log::debug!("Dependencies checked successfully.");
+    match browser_website(&args.url) {
+        Ok(dsid) => println!("{dsid}"),
+        Err(_) => todo!(),
+    }
+}
 
-    let driver = Driver::start(&mut args.port).await?;
-    log::debug!("Driver started successfully.");
+fn browser_website(url: &str) -> Result<String, Box<dyn Error>> {
+    // Define the user data directory within the user's .config directory.
+    let home_dir = env::var("HOME")?;
+    let user_data_dir = PathBuf::from(format!("{}/.config/kuvpn", home_dir));
 
-    // Wait for the WebDriver to be reachable
-    driver
-        .wait_for_webdriver(args.port, Duration::from_secs(30))
-        .await?;
-    log::debug!("WebDriver is reachable.");
+    // Ensure the directory exists
+    if !user_data_dir.exists() {
+        fs::create_dir_all(&user_data_dir)?;
+    }
 
-    let args = &mut args;
-    run_client_with_retries(driver, args, args.dsid).await?;
+    // Launch the browser with the specified user data directory.
+    let browser = Browser::new(
+        LaunchOptions::default_builder()
+            .headless(false)
+            .sandbox(false)
+            .args(vec![
+                OsString::from("--app=data:text/html,<html><body></body></html>").as_os_str(),
+                OsString::from("----new-window").as_os_str(),
+            ]) // Converts &str to OsStr
+            .user_data_dir(Some(user_data_dir)) // Set the .config/kuvpn directory
+            .build()
+            .expect("Could not find chrome-executable"),
+    )?;
 
-    log::debug!("Reached end of main.");
-    Ok(())
+    // Wait for the browser to launch and the tab to load
+    #[allow(deprecated)]
+    let tab = browser.wait_for_initial_tab()?;
+
+    // Navigate to the given URL and wait for the page to load completely
+    tab.navigate_to(url)?;
+    tab.wait_until_navigated()?;
+
+    // Run in a loop to continuously check for the DSID or storage item.
+    loop {
+        // Attempt to get the 'DSID' cookie directly.
+        let script =
+            "document.cookie.split('; ').find(row => row.startsWith('DSID='))?.split('=')[1];";
+        let remote_object = tab.evaluate(script, true)?;
+
+        if let Some(dsid_value) = remote_object.value {
+            if let Some(dsid_string) = dsid_value.as_str() {
+                return Ok(dsid_string.to_string());
+            }
+        }
+
+        // Sleep for a specified duration before running the script again.
+        thread::sleep(Duration::from_millis(100));
+    }
 }
